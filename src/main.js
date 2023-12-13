05aad5f56b9107d7adba6eab8b96784b3637353b
Ivan Zotov <vanzo16-workmail@ya.ru>
2023-12-13 22:11:37 +0300

    fix view

--- src/main.js
+++ src/main.js
@@ -1,20 +1,14 @@
 import BoardPresenter from './presenter/board-presenter.js';
-import RouteView from './view/route-view.js';
-import {render,RenderPosition} from './render.js';
 
-const siteMainElement = document.querySelector('.page-body');
-const siteSortElement = siteMainElement.querySelector('.trip-events');
-const siteInfoElement = siteMainElement.querySelector('.trip-main');
-const siteFilterElement = siteMainElement.querySelector('.trip-controls__filters');
-
+const siteSortElement = document.querySelector('.trip-events');
+const siteInfoElement = document.querySelector('.trip-main');
+const siteFilterElement = document.querySelector('.trip-controls__filters');
 
 const boardPresenter = new BoardPresenter({
+  routeContainer: siteInfoElement,
   sortContainer: siteSortElement,
-  headerContainer: siteFilterElement,
-  editContainer: siteSortElement,
+  filterContainer: siteFilterElement,
   listContainer: siteSortElement
 });
 
 boardPresenter.init();
-
-render(new RouteView(), siteInfoElement, RenderPosition.AFTERBEGIN);
